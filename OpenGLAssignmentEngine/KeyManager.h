d6923bbf1d4e114c5e11aaea6af17f7df7872cbc
나 <0329wldns@gmail.com>
2026-09-22 20:19:36 +0900

    r키로 초기화 구현

--- OpenGLAssignmentEngine/KeyManager.h
+++ OpenGLAssignmentEngine/KeyManager.h
@@ -5,44 +5,42 @@
 
 enum class KEY
 {
-    UP, DOWN, LEFT, RIGHT,
-    W, A, S, D,
-    MOUSE_L, MOUSE_R,
-    C, M, Y, G, K, T,
-    NUM_1, NUM_2, NUM_3, NUM_4,
-    ESC,
-    LAST
+	A,
+	R,
+	MOUSE_L, MOUSE_R,
+	ESC,
+	LAST
 };
 
 enum class KEY_STATE
 {
-    NONE,
-    TAP,
-    HOLD,
-    AWAY
+	NONE,
+	TAP,
+	HOLD,
+	AWAY
 };
 
 struct KeyInfo
 {
-    KEY_STATE keyState{};
-    bool prevPush{};
+	KEY_STATE keyState{};
+	bool prevPush{};
 };
 
 class KeyManager : public Singleton<KeyManager>
 {
-    friend class Singleton<KeyManager>;
+	friend class Singleton<KeyManager>;
 
 public:
-    void init();
-    void update();
+	void init();
+	void update();
 
-    KEY_STATE getKeyState(KEY _key) const { return keyInfo[(int)_key].keyState; }
-    Vector2 getMousePos() const { return curMousePos; }
-
-private:
-    KeyManager() = default;
+	KEY_STATE getKeyState(KEY _key) const { return keyInfo[(int)_key].keyState; }
+	Vector2 getMousePos() const { return curMousePos; }
 
 private:
-    vector<KeyInfo> keyInfo;
-    Vector2 curMousePos{};
+	KeyManager() = default;
+
+private:
+	vector<KeyInfo> keyInfo;
+	Vector2 curMousePos{};
 };
--- OpenGLAssignmentEngine/Scene_Assignment3.cpp
+++ OpenGLAssignmentEngine/Scene_Assignment3.cpp
@@ -10,6 +10,7 @@ uniform_int_distribution<int> intDist(1, 10000);
 uniform_real_distribution<float> realDist(0.0f, 1.0f);
 
 static const int g_sizeLimit{ 100 };
+static int g_rectCnt{};
 
 void Scene_Assignment3::update()
 {
@@ -17,13 +18,25 @@ void Scene_Assignment3::update()
 		changeScene(SCENE_TYPE::START);
 	if (KeyManager::getInstance().getKeyState(KEY::A) == KEY_STATE::TAP)
 	{
-		Rect* rect = new Rect();
-		rect->setPos(intDist(gen) % SCREEN_WIDTH, intDist(gen) % SCREEN_HEIGHT);
-		rect->setScale(10 + intDist(gen) % g_sizeLimit, 10 + intDist(gen) % g_sizeLimit);
-		rect->setColor(realDist(gen), realDist(gen), realDist(gen));
+		system("cls");
+		if (g_rectCnt < 10)
+		{
+			Vector2 pos{ intDist(gen) % SCREEN_WIDTH, intDist(gen) % SCREEN_HEIGHT };
+			Vector2 scale{ 10 + intDist(gen) % g_sizeLimit, 10 + intDist(gen) % g_sizeLimit };
+			Color col{ realDist(gen), realDist(gen), realDist(gen) };
 
-		createObject(rect, OBJECT_GROUP::RECT);
+			Rect* rect = new Rect();
+			rect->setPos(pos);
+			rect->setScale(scale);
+			rect->setColor(col);
+
+			createObject(rect, OBJECT_GROUP::RECT);
+			++g_rectCnt;
+		}
+		else cout << "사각형이 너무 많음!" << endl;
+		cout << "사각형 개수: " << g_rectCnt << endl;
 	}
+	if (KeyManager::getInstance().getKeyState(KEY::R) == KEY_STATE::TAP) reset();
 
 	Scene::update();
 }
